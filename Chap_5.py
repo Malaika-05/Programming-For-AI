d02e0517c73cbc0760129de95eb015b1ad52e087
Malaika Taqveem <malaikataqveem@gmail.com>
2026-02-26 23:12:02 +0500

    Chapter 05 (Dictionaries and Sets)

--- Chap_5.py
+++ Chap_5.py
@@ -1,72 +1,72 @@
-# s = {
-#     "Name" : "Malaika Taqveem ",
-#     "Roll no ": 83,
-#     "Subjects ": ["AI","Database","statistics","Python"]
-# }
-# print(s.keys()) #dict_keys(['Name', 'Roll no ', 'Subjects '])
-# print(s.values()) #dict_values(['Malaika Taqveem ', 83, ['AI', 'Database', 'statistics', 'Python']])
-# print(s.items()) #dict_items([('Name', 'Malaika Taqveem '), ('Roll no ', 83), ('Subjects ', ['AI', 'Database', 'statistics', 'Python'])])
-# print(s.get("Name")) #Malaika Taqveem
+s = {
+    "Name" : "Malaika Taqveem ",
+    "Roll no ": 83,
+    "Subjects ": ["AI","Database","statistics","Python"]
+}
+print(s.keys()) #dict_keys(['Name', 'Roll no ', 'Subjects '])
+print(s.values()) #dict_values(['Malaika Taqveem ', 83, ['AI', 'Database', 'statistics', 'Python']])
+print(s.items()) #dict_items([('Name', 'Malaika Taqveem '), ('Roll no ', 83), ('Subjects ', ['AI', 'Database', 'statistics', 'Python'])])
+print(s.get("Name")) #Malaika Taqveem
 
 
 # Sets
-# a = set()
-# print(type(a))
-# a.add(3)
-# a.add(3)
-# a.add(7)
-# a.add(5)
-# a.add(8)
-# print(a) #{8, 3, 5, 7}
+a = set()
+print(type(a))
+a.add(3)
+a.add(3)
+a.add(7)
+a.add(5)
+a.add(8)
+print(a) #{8, 3, 5, 7}
 
-# b = {8, 3, 5, 7}
-# print(len(b)) # 4
-# b.remove(5) #{8, 3, 7}
-# b.pop() #{3, 7} remove random value in the set
-# b.clear() #set()
-# print(b)
-# c = {1,2,3,4,5,6}
-# d = {6,7,8,9}
-# print(c.union(d)) #{1, 2, 3, 4, 5, 6, 7, 8, 9}
-# print(c.intersection(d)) #{6}
+b = {8, 3, 5, 7}
+print(len(b)) # 4
+b.remove(5) #{8, 3, 7}
+b.pop() #{3, 7} remove random value in the set
+b.clear() #set()
+print(b)
+c = {1,2,3,4,5,6}
+d = {6,7,8,9}
+print(c.union(d)) #{1, 2, 3, 4, 5, 6, 7, 8, 9}
+print(c.intersection(d)) #{6}
 
 
 # Chapter 5 practice set
 
 #Write a program to create a dictionary of urdu words with values as their English translation. Provide user with an option to look it up!
 
-# words = {
-#     "house": "Ghar",
-#     "street": "Gali",
-#     "tea": "Chai",
-#     "water": "Paani",
-#     "cat": "Billi",
-#     "parrot": "Mithu"
-# }
+words = {
+    "house": "Ghar",
+    "street": "Gali",
+    "tea": "Chai",
+    "water": "Paani",
+    "cat": "Billi",
+    "parrot": "Mithu"
+}
 
-# choice = input("Enter your choice: ").lower()
-# print(words[choice])
+choice = input("Enter your choice: ").lower()
+print(words[choice])
 
 # Write a program to input eight numbers from the user and display all the unique numbers (once).= 
-# numbers = set()
-# print(type(numbers))
-# num1 = int(input("Enter the number : "))
-# numbers.add(num1)
-# num2 = int(input("Enter the number : "))
-# numbers.add(num2)
-# num3 = int(input("Enter the number : "))
-# numbers.add(num3)
-# num4 = int(input("Enter the number : "))
-# numbers.add(num4)
-# num5 = int(input("Enter the number : "))
-# numbers.add(num5)
-# num6 = int(input("Enter the number : "))
-# numbers.add(num6)
-# num7 = int(input("Enter the number : "))
-# numbers.add(num7)
-# num8 = int(input("Enter the number : "))
-# numbers.add(num8)
-# print(numbers)
+numbers = set()
+print(type(numbers))
+num1 = int(input("Enter the number : "))
+numbers.add(num1)
+num2 = int(input("Enter the number : "))
+numbers.add(num2)
+num3 = int(input("Enter the number : "))
+numbers.add(num3)
+num4 = int(input("Enter the number : "))
+numbers.add(num4)
+num5 = int(input("Enter the number : "))
+numbers.add(num5)
+num6 = int(input("Enter the number : "))
+numbers.add(num6)
+num7 = int(input("Enter the number : "))
+numbers.add(num7)
+num8 = int(input("Enter the number : "))
+numbers.add(num8)
+print(numbers)
 
 # Can we have a set with 18 (int) and '18' (str) as a value in it?
 z = set()
